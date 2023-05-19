c8fc0baf670cca233781c91927c1fb230193908c
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-05-19 16:02:45 +0530

    Some Bugs Fixed

--- frontend/src/navigation/index.jsx
+++ frontend/src/navigation/index.jsx
@@ -1,35 +1,30 @@
-import React, {useState, useEffect} from 'react';
-import Navbar from './../components/Navbar';
-import Footer from './../components/Footer';
-import AuthRoute from './authRoute';
-import UserRoute from './userRoute';
+import React, { useState, useEffect } from "react";
+import Navbar from "./../components/Navbar";
+import Footer from "./../components/Footer";
+import AuthRoute from "./authRoute";
+import UserRoute from "./userRoute";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from  'react-toastify';
-import auth from '../services/authServices';
-  
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import auth from "../services/authServices";
 
+export default function RootNavigation() {
+  const [user, setUser] = useState([]);
 
-export default function RootNavigation(){
-    const [user, setUser] = useState([]);
+  useEffect(() => {
+    const user = auth.getCurrentUser();
+    setUser(user);
+  }, []);
 
-    useEffect(()=> {
-        const user = auth.getCurrentUser();
-        setUser(user);
-    },[]);
+  console.log("user", user);
 
-    console.log("user", user);
-    
-
-
-    return (
-        <>
-        <ToastContainer />
-        <Navbar user={user} />
-        {user ?  <UserRoute /> :  <AuthRoute /> }
-        <Footer />
-        </>
-    )
-};
-
+  return (
+    <>
+      <ToastContainer />
+      <Navbar user={user} />
+      {user ? <UserRoute /> : <AuthRoute />}
+      <Footer />
+    </>
+  );
+}
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,44 +1,45 @@
-import React, {useState} from 'react';
-import Joi from 'joi-browser';
-import Input from './../common/input';
+import React, { useState } from "react";
+import Joi from "joi-browser";
+import Input from "./../common/input";
 import * as userService from "../services/userService";
-import auth from '../services/authServices';
-
+import auth from "../services/authServices";
 
 const Signup = () => {
-   const [account, setAccount] = useState({
+  const [account, setAccount] = useState({
     name: "",
-    email: "", 
-    password: "", 
-   });
+    email: "",
+    password: "",
+  });
 
-   const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-   const schema = {
-     name : Joi.optional().allow(""),
-     email : Joi.string().email({tlds: { allow: false}, }).required().label("Email"),
-     password: Joi.string().required().min(5).label("Password"),
-   }
+  const schema = {
+    name: Joi.optional().allow(""),
+    email: Joi.string()
+      .email({ tlds: { allow: false } })
+      .required()
+      .label("Email"),
+    password: Joi.string().required().min(5).label("Password"),
+  };
 
-
-   const validate = () => {
+  const validate = () => {
     const options = { abortEarly: false };
     const { error } = Joi.validate(account, schema, options);
-    if(!error) return null;
- 
+    if (!error) return null;
+
     const errors = {};
     for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
-   };  
- 
-   const validateProperty = ({name, value}) => {
-     const obj = { [name] : value};
-     const schemas = { [name] : schema[name]};
-     const {error} = Joi.validate(obj, schemas);
-     return error ? error.details[0].message :  null;
-   };
+  };
 
-   const handleSubmit = async (e) => {
+  const validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schemas = { [name]: schema[name] };
+    const { error } = Joi.validate(obj, schemas);
+    return error ? error.details[0].message : null;
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const errors = validate();
@@ -47,8 +48,8 @@ const Signup = () => {
     if (errors) return;
     try {
       const response = await userService.register(account);
-      auth.loginWithJwt(response.headers["x-auth-token"]);
-      window.location = "/dashboard";      
+      auth.loginWithJwt(response.data.token);
+      window.location = "/dashboard";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...errors };
@@ -56,78 +57,67 @@ const Signup = () => {
         setErrors({ errors });
       }
     }
-  };  
-  
+  };
 
-  const handleChange= ({ currentTarget : input}) => {
-
-    const error = {...errors};
+  const handleChange = ({ currentTarget: input }) => {
+    const error = { ...errors };
     const errorMessage = validateProperty(input);
-    if(errorMessage) error[input.name]= errorMessage;
+    if (errorMessage) error[input.name] = errorMessage;
     else delete error[input.name];
 
-    const accounts = {...account};
+    const accounts = { ...account };
     accounts[input.name] = input.value;
     setAccount(accounts);
     setErrors(error);
-	};
+  };
 
   const { name, email, password } = account;
 
   return (
     <>
-    <div className="container vh-100">
-    <form onSubmit={handleSubmit}>
-
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-         <h5>Sign up for New User</h5>
-        </div>
-        <div className="col-md-6 offset-md-3">
-        <Input 
-            name="name"
-            label="Username"
-            value={name}
-            error={errors.name}
-            onChange={handleChange} 
-            />
-
-        </div>
-        <div className="col-md-6 offset-md-3">
-        <Input 
-            name="email"
-            label="Email"
-            value={email}
-            error={errors.email}
-            onChange={handleChange} 
-            />
-
-        </div>
-        <div className="col-md-6 offset-md-3">
-        <Input 
-            name="password"
-            label="Password"
-            value={password}
-            error={errors.password}
-            onChange={handleChange} 
-            />
-
-        </div>
-        <div className="col-md-6 offset-md-3 mt-3">
-           <button type="submit" class="btn btn-outline-primary">Submit</button>
-        </div>
-
+      <div className="container vh-100">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <h5>Sign up for New User</h5>
+            </div>
+            <div className="col-md-6 offset-md-3">
+              <Input
+                name="name"
+                label="Username"
+                value={name}
+                error={errors.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-md-6 offset-md-3">
+              <Input
+                name="email"
+                label="Email"
+                value={email}
+                error={errors.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-md-6 offset-md-3">
+              <Input
+                name="password"
+                label="Password"
+                value={password}
+                error={errors.password}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-md-6 offset-md-3 mt-3">
+              <button type="submit" class="btn btn-outline-primary">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-
- 
-      </form>
-
-    </div>
-
-
-
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
