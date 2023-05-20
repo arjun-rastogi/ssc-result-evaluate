2209d42e6482e7810b79de2549746fdb955e19b7
arjun-rastogi <71251807+arjun-rastogi@users.noreply.github.com>
2023-05-20 21:43:12 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,6 +4,8 @@ const jwt = require("jsonwebtoken");
 
 exports.Signup = async (req, res, next) => {
   try {
+    const { name, email, password } = req.body; // Assuming you're extracting the values from the request body
+
     let hashedPassword = await bcrypt.hash(password, 10);
     let query = "INSERT INTO USERS (name, email, password) VALUES (?, ?, ?)";
     connection.query(query, [name, email, hashedPassword], (err, results) => {
