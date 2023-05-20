f29fc7f354b4faae497ced15ae16e6a5ddd9244d
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-05-20 12:58:22 +0530

    Backend Re Initialised

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const connection = require("./connection");
-const authRoute = require('./routes/authentication');
+const authRoute = require("./routes/authentication");
 
 const app = express();
 
-app.use(express.urlencoded({extended:  true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-
 // Routing for our product file.
-app.use('/api', authRoute);
+app.use("/api", authRoute);
 
 module.exports = app;
